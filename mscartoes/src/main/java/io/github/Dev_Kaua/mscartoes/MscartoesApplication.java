b33f33af323b4963f7358a577e282dcdd6e1eea0
Kauã Reis Rodrigues <kauadragon036@gmail.com>
2025-06-29 13:19:41 -0300

    Resolução de erro 500 relacionado a comunicação do gateway com os microsserviços -> relacionado ao erro na geração da porta do gateway que não estava sendo gerada uma porta localhost:8080 como deveria. -> o erro foi solucionado alterando as configurações do application.yml dos microsserviços, além de inserir o @EnableDiscoveryClient nas classes principais.

--- mscartoes/src/main/java/io/github/Dev_Kaua/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/io/github/Dev_Kaua/mscartoes/MscartoesApplication.java
@@ -3,8 +3,10 @@ package io.github.Dev_Kaua.mscartoes;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableRabbit
 public class MscartoesApplication {
 
